c585863dc9728d4c7951d83c6dc26802ad35fc6e
suhaasya <suhaskhobragade19@gmail.com>
2024-12-26 22:14:37 +0530

    adds other recurssions code

--- 03-recursion/main.go
+++ 03-recursion/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func print(times int, name string) {
 	if times == 0 {
@@ -37,9 +39,50 @@ func printNNumbersRev2(n int) {
 	fmt.Println(n)
 }
 
+func printSumNNumberss(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	return n + printSumNNumberss(n-1)
+}
+
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func isPalindrome(i int, str string) bool {
+	if i >= len(str) {
+		return true
+	}
+	if str[i] != str[len(str)-1-i] {
+		return false
+	}
+	return isPalindrome(i+1, str)
+}
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
 func main() {
 	// print(5, "suhas")
 	// printNNumbers(12)
 	// printNNumbersRev(12)
-	printNNumbersRev2(12)
+	// printNNumbersRev2(12)
+	// n := printSumNNumberss(3)
+	// strings.len
+	// fmt.Println(factorial(4))
+	// fmt.Println(isPalindrome(0, "madam"))
+	// fmt.Println(isPalindrome(0, "madman"))
+	fmt.Println(fibonacci(10))
 }
